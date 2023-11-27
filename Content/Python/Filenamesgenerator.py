db50dbffbc89f277f4a3fdd9ddf5ad8902e89796
Juan Maldini <juan.a.maldini@gmail.com>
2023-11-27 12:49:46 +0100

    up

--- Content/Python/Filenamesgenerator.py
+++ Content/Python/Filenamesgenerator.py
@@ -1,9 +1,8 @@
 import os
 import csv
 
-# Ruta del directorio donde se encuentran los archivos .udatasmith
-ds_folder = r'C:\Users\juanm\XL Consulting AU pty Ltd\02 - FY24 PROJECTS - Documents\2024031 - CrestLink Business Pk (Hindmarsh QLD)\03 - 3D & 4D\3D\XL_Exported'
-py_folder = r'C:\Users\juanm\Documents\GitHub\XL_Template\Content\Python'
+ds_folder = 'C:\Users\juanm\OneDrive\Projects\XL\01 - Model\LiveLinkTest_V03\XL_Exported'
+py_folder = 'C:\Users\juanm\Documents\GitHub\XL_Template\Content\Python'
 
 # Define la función para crear un archivo CSV con los nombres de archivos .udatasmith
 def generate_csv(ds_folder, py_folder):
@@ -15,12 +14,12 @@ def generate_csv(ds_folder, py_folder):
     
     # Escribe los nombres de archivo en un archivo CSV
     with open(csv_file_path, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)  # Asegúrate de que todas las entradas estén entre comillas
-        writer.writerow(['---', 'Filename'])  # Escribe el encabezado personalizado
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerow(['---', 'Filename'])
         for index, name in enumerate(file_names, start=1):
-            writer.writerow([f'{index:02}', name])  # Escribe los nombres con índice
+            writer.writerow([f"{index:02}", name])
 
     print(f"CSV file created at: {csv_file_path}")
 
-# Llama a la función
+# Ejemplo de cómo podrías llamar a esta función directamente con rutas codificadas
 generate_csv(ds_folder, py_folder)
