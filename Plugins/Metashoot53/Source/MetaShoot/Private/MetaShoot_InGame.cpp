e7de1e4394d9569e26e1568579acde5f368a7d58
Juan Maldini <juan.a.maldini@gmail.com>
2023-12-01 19:04:03 +0100

    e

--- Plugins/Metashoot53/Source/MetaShoot/Private/MetaShoot_InGame.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright 2023, VINZI Studio S.L. All rights reserved
-
-
-#include "MetaShoot_InGame.h"
-
--- Plugins/Metashoot53/Source/MetaShoot/Private/MetaShoot_Preset.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2023, VINZI Studio S.L. All rights reserved
-
-
-#include "MetaShoot_Preset.h"
-
-AMetaShoot_Preset::AMetaShoot_Preset()
-{
-    // Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
-}
